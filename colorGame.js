8641c531e856cb2adb5cde3ea5daaab24c563919
Johnny Tsang <jaytsang9@gmail.com>
2019-08-10 14:21:24 -0400

    added reset button for play again features, reset button will regenerate 6 new random colors while also toggling the h1 background color back to black

--- colorGame.js
+++ colorGame.js
@@ -1,9 +1,25 @@
 var colors = generateRandomColors(6)
-
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+	//generate all new colors
+	colors = generateRandomColors(6);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//change colorDisplay to match picked color
+	colorDisplay.textContent = pickedColor;
+	//change colors of squares
+	for (var i = 0; i < squares.length; i++){
+		// add initial colors to squares
+		squares[i].style.backgroundColor = colors[i];
+	};
+	h1.style.background = "#232323";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -15,7 +31,9 @@ for (var i = 0; i < squares.length; i++){
 		var clickedColor = this.style.backgroundColor;
 		//compare clicked color to color display
 		if (clickedColor === pickedColor){
-            messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again?"
+			messageDisplay.textContent = "Correct!";
+			h1.style.backgroundColor = clickedColor;
             changeColors(clickedColor);
 		}else {
             this.style.background = "#232323";
